7f05faeb6d79536d0000a3af272b37ad12c25d84
Nir-Agiv <niragiv1@gmail.com>
2024-11-07 14:09:42 +0200

    Server is up and running

--- routes/flightRoutes.js
+++ routes/flightRoutes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const flightController = require('../controllers/flightController');
 
+
 // GET all flights
 router.get('/', flightController.getAllFlights);
 
