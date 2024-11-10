9091de54007fd0a29544180bf36f29bb04d99ec0
Nir-Agiv <niragiv1@gmail.com>
2024-11-10 13:49:16 +0200

    Server is connected to DB without crashing

--- config/db.js
+++ config/db.js
@@ -2,16 +2,13 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log(`MongoDB connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1); // Exit process with failure
-  }
+  mongoose.connect('mongodb://localhost:27017/flightclub')
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 };
 
 module.exports = connectDB;
