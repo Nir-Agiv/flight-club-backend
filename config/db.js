961eca34c6c2178fa1f67d04341a568cdbe2f56e
Tamir Beneden <tamirbe2@ac.sce.ac.il>
2024-12-03 00:01:38 +0200

    fix mongo URL

--- config/db.js
+++ config/db.js
@@ -12,11 +12,7 @@ const mongoURI = `mongodb://${dbHost}:${dbPort}/${dbName}`;
 // פונקציה לחיבור למסד הנתונים
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 30000, // 30 seconds
-    });
+    await mongoose.connect(mongoURI);
     console.log('MongoDB connected');
     await populateDB();
   } catch (error) {
